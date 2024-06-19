f2bc3629cc0e0f6c13825225470cc0d98588e6c2
Nino Alexander Vinalon <navinalon@gmail.com>
2024-06-19 20:28:09 +0000

    remove other const in DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() { 
     const buttons = document.getElementsByTagName("button");
-    /*
-    const buttonBlue =document.getElementsByClassName("button-blue");
-    const buttonRed = document.getElementsByClassName("button-red");
-    const buttonYellow = document.getElementsByClassName("button-yellow");
-    const buttonGreen = document.getElementsByClassName("button-green");
-    const buttonPink = document.getElementsByClassName("button-pink");
-    const buttonOrange = document.getElementsByClassName("button-orange");
-    */
     const colorBox = documment.getElementsByClassName("color-box")[0];
     const choices = ["blue", "red", "yellow", "green", "pink", "orange",];
 
