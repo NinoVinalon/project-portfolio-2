696339ba785e4e5ed8b379e0023f5503649ac75a
Nino Alexander Vinalon <navinalon@gmail.com>
2024-06-19 19:22:10 +0000

    Add choices and constant button green

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const buttonBlue =document.getElementsByClassName("button-blue");
     const buttonRed = document.getElementsByClassName("button-red");
     const buttonYellow = document.getElementsByClassName("button-yellow");
+    const buttonGreen = document.getElementsByClassName("button-green");
     const buttonPink = document.getElementsByClassName("button-pink");
     const buttonOrange = document.getElementsByClassName("button-orange");
+    const choices = ["blue", "red", "yellow", "green", "pink", "orange",];
 
 })
 
