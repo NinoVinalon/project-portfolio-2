d8a9803bc988665c652d828fb8118ae9c7fa90b4
Nino Alexander Vinalon <navinalon@gmail.com>
2024-06-19 19:19:01 +0000

    Add function, event listener and constants

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.getElementsByTagName("button");
+    const buttonBlue =document.getElementsByClassName("button-blue");
+    const buttonRed = document.getElementsByClassName("button-red");
+    const buttonYellow = document.getElementsByClassName("button-yellow");
+    const buttonPink = document.getElementsByClassName("button-pink");
+    const buttonOrange = document.getElementsByClassName("button-orange");
+
+})
+
+function runGame() {
+
+}
+function incrementScore() {
+
+}
+function reductionScore() {
+
+}
