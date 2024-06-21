6f8aba8a77ab6bae243666f6daff3debf78f5131
Nino Alexander Vinalon <navinalon@gmail.com>
2024-06-21 13:22:21 +0000

    add rules in html, style it on css and add event listener in javascipt for the rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.getElementsByTagName("button");
     const colorBox = document.getElementById("color-box");
     const choices = ["blue", "red", "yellow", "green", "pink", "orange",];
     const score = document.getElementById("score");
     let currentScore = parseInt(score.textContent);
-    
+
     /* add event listener to all buttons */
     for (let button of buttons) {
         button.addEventListener("click", function () {
@@ -13,46 +13,51 @@ document.addEventListener("DOMContentLoaded", function() {
             runGame(playerChoice);
         });
     }
-    
-function runGame(playerChoice) {
-    let computerChoice = Math.floor(Math.random() * choices.length);
-    console.log(computerChoice);
 
-    let color = choices[computerChoice];
-    console.log(color);
+    /* event listener for rules button */
+    document.getElementById("rules").addEventListener("click", function () {
+        alert("Game Rules:\n1. Click a color button to choose a color.\n2. A random color will be displayed in the box.\n3. If the displayed color matches your choice, you win 50 pts! \n4. If the displayed color did not match, you lose 10 points. \n5. Once your score goes down to ZERO, its Game Over!")
+    })
 
-    colorBox.style.backgroundColor = color;
-    colorBox.setAttribute('aria-label', `Color: ${computerChoice}`);
-    console.log(currentScore);
+    function runGame(playerChoice) {
+        let computerChoice = Math.floor(Math.random() * choices.length);
+        console.log(computerChoice);
 
-    setTimeout(function() { 
-        if (playerChoice !== color) {
-            currentScore -= 10;
-            score.textContent = currentScore;
-            console.log(currentScore);
-            alert (`Incorrect the color is ${color}`);
-        } else {
-            currentScore += 50;
-            score.textContent = currentScore;
-            console.log(currentScore);
-            alert ("Correct");
-        }
+        let color = choices[computerChoice];
+        console.log(color);
+
+        colorBox.style.backgroundColor = color;
+        colorBox.setAttribute('aria-label', `Color: ${computerChoice}`);
+        console.log(currentScore);
+
+        setTimeout(function () {
+            if (playerChoice !== color) {
+                currentScore -= 10;
+                score.textContent = currentScore;
+                console.log(currentScore);
+                alert(`Incorrect the color is ${color}`);
+            } else {
+                currentScore += 50;
+                score.textContent = currentScore;
+                console.log(currentScore);
+                alert("Correct");
+            }
 
             if (currentScore <= 0) {
                 currentScore = 0;
-                score.textContent = currentScore;     
+                score.textContent = currentScore;
                 end();
-                alert ("Game Over! Your score is 0");
+                alert("Game Over! Your score is 0");
                 score.parentElement.textContent = "GAME OVER!";
             }
-          
-    }, 300); 
 
-    function end() {
-        for (let button of buttons) {
-            button.disabled = true;
+        }, 300);
+
+        function end() {
+            for (let button of buttons) {
+                button.disabled = true;
+            }
         }
+
     }
-    
-}  
 });
